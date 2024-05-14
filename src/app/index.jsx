c18e232ab8f7f1a49330e0d255bd75f79537767c
lisperey <pilrss16@gmail.com>
2024-05-14 16:50:30 -0300

    feat: Configurado graphql

--- src/app/index.jsx
+++ src/app/index.jsx
@@ -3,29 +3,36 @@ import { FlatList, StyleSheet, Text, View, ActivityIndicator } from 'react-nativ
 import exercises from '../../assets/data/exercises.json';
 import ExerciseListIem from '../../src/components/ExerciseListItem';
 import { useQuery } from '@tanstack/react-query';
-import { gql, request } from 'graphql-request';
+import { gql, request } from 'graphql-request'
 
-const url = `https://kararval.us-east-a.ibm.stepzen.net/api/wiggly-labradoodle/graphql`;
+const url = `https://kararval.us-east-a.ibm.stepzen.net/api/khaki-nightingale/graphql`;
 const exercisesQuery = gql`
-    query Exercise($muscle: String , $name: String) {
-        exercises(muscle: $muscle, name: $name) {
-        name
-        muscle
-        }
+  query exercises($muscle: String, $name: String, $offset: Int) {
+    exercises(muscle: $muscle, name: $name, offset: $offset) {
+      name
+      muscle
+      equipment
     }
+  }
 `;
-
 
 export default function App() {
     const { data, isLoading, error } = useQuery({
         queryKey: ['exercises'],
-        queryFn: () => {
-            return request(url, exercisesQuery);
+        queryFn: async () => {
+            return request({
+                url, 
+                document: exercisesQuery, 
+                requestHeaders: { 
+                    Authorization: 
+                    'apikey kararval::local.net+1000::71d8fbcb7f90d921c63fe9c3e104bc2893c184a401815dfce06e63a72cfaa208' 
+                },
+                });
         }
     });
+    console.log(data)
     if (isLoading) {
         return <ActivityIndicator />
-
     }
     if(error){
         return <Text>Erro ao buscar exercícios</Text>
@@ -33,7 +40,7 @@ export default function App() {
     return (
         <View style={styles.container}>
             <FlatList
-                data={data}
+                data={data?.exercises}
                 contentContainerStyle={{ gap: 5 }}
                 keyExtractor={(item, index) => item.name + index}
                 renderItem={({ item, index }) => <ExerciseListIem item={item} />}
